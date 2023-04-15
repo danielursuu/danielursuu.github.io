fe45f89d819c1f6295fca8ab4949171d77b62dab
Daniel Ursu <57760647+danielursu@users.noreply.github.com>
2023-04-15 23:47:40 +0300

    added gsap animations

--- script.js
+++ script.js
@@ -17,17 +17,42 @@ const masonry = new Masonry(grid, masonryOptions);
 const images = grid.querySelectorAll("img");
 let imagesLoaded = 0;
 
-const updateLayout = () => {
-  imagesLoaded++;
-  if (imagesLoaded === images.length) {
-    masonry.layout();
-  }
-};
-
 images.forEach((image) => {
+  const updateLayout = () => {
+    imagesLoaded++;
+    if (imagesLoaded === images.length) {
+      masonry.layout();
+      animateGridItems();
+    }
+  };
+
   if (image.complete) {
     updateLayout();
   } else {
     image.addEventListener("load", updateLayout);
   }
 });
+
+function animateGridItems() {
+  const gridItems = grid.querySelectorAll(".grid-item");
+  gsap.set(gridItems, { autoAlpha: 0 });
+  gsap.to(gridItems, {
+    autoAlpha: 1,
+    duration: 1,
+    stagger: 0.15,
+    ease: "power2.out",
+  });
+}
+
+function animateHeader() {
+  const header = document.querySelector("header");
+  gsap.from(header, {
+    y: -50,
+    autoAlpha: 0,
+    duration: 1,
+    ease: "power2.out",
+  });
+}
+
+// Call the animateHeader function to animate the header
+animateHeader();
